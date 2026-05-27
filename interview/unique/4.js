418c8d57fe1f9c5497212a864f6559a10d5e1a16
YIHAOLAI <501575435@qq.com>
2026-05-27 20:54:18 +0800

    大厂面试题：数组去重

---
+++ interview/unique/4.js
@@ -0,0 +1,25 @@
+/**
+ * @func 数组去重
+ * @param {Array} arr 数组
+ * @returns {Array} 去重后的数组
+ * @author yh
+ * @date 2026-05-27
+ */
+//排序法
+function unique(arr) {
+//参数校验 不是数组，返回空数组
+    if (!Array.isArray(arr)) {
+        console.error('type error');
+        return [];
+    }
+    arr=arr.sort();
+    let res=[];
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]!==arr[i+1]){
+            res.push(arr[i]);
+        }
+    }
+    return res;
+
+}
+console.log(unique([1, 2, 3, 2, 5])); // [1, 2, 3, 5]
---
+++ interview/unique/5.js
@@ -0,0 +1,28 @@
+/**
+ * @func 数组去重
+ * @param {Array} arr 数组
+ * @returns {Array} 去重后的数组
+ * @author yh
+ * @date 2026-05-27
+ */
+//hash表法
+function unique(arr) {
+//参数校验 不是数组，返回空数组
+    if (!Array.isArray(arr)) {
+        console.error('type error');
+        return [];
+    }
+   let res = [];
+   obj = {};
+for (let i = 0; i < arr.length; i++) {
+    if(!obj[arr[i]]) {
+        res.push(arr[i]);
+        obj[arr[i]] = 1;
+    }
+    else{
+        obj[arr[i]]++;
+    }
+  }
+  return res;
+}
+console.log(unique([1, 2, 3, 2, 5])); // [1, 2, 3, 5]
